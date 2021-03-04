96a0cdbfc249725187ec41553eba27d7caafa497
Mukhammad <Fengints@yandex.ru>
2021-03-04 13:29:21 +0600

    dekete utils

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF.Tools;
 using WPF.ViewModels;
 
 namespace WPF
@@ -115,36 +116,6 @@ namespace WPF
         }
     }
 
-
-    public class StatusInfo : INotifyPropertyChanged
-    {
-        public StatusInfo()
-        {
-
-        }
-        public StatusInfo(int statusMin, int statusMax)
-        {
-            this.StatusMin = statusMin;
-            this.StatusMax = statusMax;
-        }
-        private int status;
-        private int statusMax;
-        private int statusMin;
-
-        public int CurrentStatus { get => status; set { status = value; OnPropertyChanged(nameof(CurrentStatus)); } }
-        public int StatusMin { get=> statusMin; set { statusMin = value; OnPropertyChanged(nameof(StatusMin)); } }
-        public int StatusMax { get => statusMax; set { statusMax = value; OnPropertyChanged(nameof(StatusMax)); } }
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    }
 
 
 }
---
+++ WPF/Tools/StatusInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Tools
+{
+    public class StatusInfo : INotifyPropertyChanged
+    {
+        public StatusInfo()
+        {
+
+        }
+        public StatusInfo(int statusMin, int statusMax)
+        {
+            this.StatusMin = statusMin;
+            this.StatusMax = statusMax;
+        }
+        private int status;
+        private int statusMax;
+        private int statusMin;
+
+        public int CurrentStatus { get => status; set { status = value; OnPropertyChanged(nameof(CurrentStatus)); } }
+        public int StatusMin { get => statusMin; set { statusMin = value; OnPropertyChanged(nameof(StatusMin)); } }
+        public int StatusMax { get => statusMax; set { statusMax = value; OnPropertyChanged(nameof(StatusMax)); } }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
